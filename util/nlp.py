d455df5802d2e6829e3075bc6011550e3136b5a5
Evgeny Ivanov <tyz910@gmail.com>
2017-11-03 20:08:54 +0300

    clean debug code

--- util/nlp.py
+++ util/nlp.py
@@ -295,9 +295,6 @@ class ParagraphQuestion(object):
             self.answer = self.answer.strip(' .?')
 
             matches = [m.start() for m in re.finditer(re.escape(self.answer.lower()), self.paragraph.text.lower())]
-            if len(matches) == 0:
-                print([self.answer.lower(), self.paragraph.text.lower()])
-
             self.answer_start = matches[answer_pos]
             self.answer_end = self.answer_start + len(self.answer)
             self.answer_start_span, self.answer_end_span = self.paragraph.find_answer_span(self.answer_start, self.answer_end)
